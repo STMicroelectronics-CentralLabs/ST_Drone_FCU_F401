3df1dbf6302cc0ad1bd6427f2d39a32c12e4c131
teomaras76 <teo.maravita@gmail.com>
2018-03-17 15:45:26 +0900

    * New official Release with BLE remocon (new Android app available).
    Major new features:
    - Drone controlled by BLE app.
    - In case BLE connectivity is used, Sensor calibration procedure and Arming/Disarming commands sent by the app.
    - Disarming and motor off in case of BLE connectivity lost.
    - Datalog of FCU data sent by BLE (Motion MEMS sensor raw data, Pressure sensor raw data, Magnetometer sensor raw data, Battery level, RSSI level FCU side, Arming status)
    - LED1 and LED2 ON when sensor calibration done and motor armed

--- STM32 FW Project/Official release with BLE Remocon - 170318/Inc/filter.h
+++
@@ -1,9 +0,0 @@
-#ifndef _FILTER_H_
-#define _FILTER_H_
-
-#include "ahrs.h"
-
-void two_order_IIR(AxesRaw_TypeDef *y, AxesRaw_TypeDef *y_p1, AxesRaw_TypeDef *y_p2, AxesRaw_TypeDef *x, AxesRaw_TypeDef *x_p1, AxesRaw_TypeDef *x_p2);
-
-#endif
-
