3061f9d87226a263ada0ab54124f106fa81a3e37
messinajo <giuseppe.messina@gmail.com>
2018-04-16 15:23:39 +0200

    Added missing file in Official release BLE Remocon 170318

--- STM32 FW Project/BLE Remocon Beta release 301117/Src/main.c
+++ STM32 FW Project/BLE Remocon Beta release 301117/Src/main.c
@@ -411,7 +411,7 @@ int32_t BytesToWrite;
     /* USER CODE BEGIN 3 */    
     if(HCI_ProcessEvent) {
           HCI_ProcessEvent=0;
-          HCI_Process();
+          HCI_Process(); // Start the BLE buffer enqueue
     }
     
     if(set_connectable){
